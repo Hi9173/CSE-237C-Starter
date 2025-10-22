89490caf83444707912a1416140954895b45fc86
Justin <zeh003@ucsd.edu>
2025-10-21 19:48:00 -0700

    2c

--- project_files/project2/cordic/HLS/cordic/cordiccart2pol.cpp
+++ project_files/project2/cordic/HLS/cordic/cordiccart2pol.cpp
@@ -1,28 +1,30 @@
 #include "cordiccart2pol.h"
 #include <math.h>
 
-data_t Kvalues[NO_ITER] = {1, 0.500000000000000, 0.250000000000000, 0.125000000000000, 0.0625000000000000, 0.0312500000000000, 0.0156250000000000, 0.00781250000000000, 0.00390625000000000, 0.00195312500000000, 0.000976562500000000, 0.000488281250000000, 0.000244140625000000, 0.000122070312500000, 6.10351562500000e-05, 3.05175781250000e-05};
+// CORDIC rotation tables - using table_t type for experimentation
+table_t Kvalues[NO_ITER] = {1, 0.500000000000000, 0.250000000000000, 0.125000000000000, 0.0625000000000000, 0.0312500000000000, 0.0156250000000000, 0.00781250000000000, 0.00390625000000000, 0.00195312500000000, 0.000976562500000000, 0.000488281250000000, 0.000244140625000000, 0.000122070312500000, 6.10351562500000e-05, 3.05175781250000e-05};
 
-data_t angles[NO_ITER] = {0.785398163397448, 0.463647609000806, 0.244978663126864, 0.124354994546761, 0.0624188099959574, 0.0312398334302683, 0.0156237286204768, 0.00781234106010111, 0.00390623013196697, 0.00195312251647882, 0.000976562189559320, 0.000488281211194898, 0.000244140620149362, 0.000122070311893670, 6.10351561742088e-05, 3.05175781155261e-05};
+table_t angles[NO_ITER] = {0.785398163397448, 0.463647609000806, 0.244978663126864, 0.124354994546761, 0.0624188099959574, 0.0312398334302683, 0.0156237286204768, 0.00781234106010111, 0.00390623013196697, 0.00195312251647882, 0.000976562189559320, 0.000488281211194898, 0.000244140620149362, 0.000122070311893670, 6.10351561742088e-05, 3.05175781155261e-05};
 
 
-void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta)
+void cordiccart2pol(data_t x, data_t y, data_t * r, data_t * theta)
 {
  data_t current_x = x;
  data_t current_y = y;
- data_t current_theta = data_t(0);
+ data_t current_theta = 0;
  
- data_t gain = data_t(0.607);
+ data_t gain = 0.607;
  
  data_t x_new, y_new;
- if (current_y < data_t(0)) {
-    x_new = -current_y;
-    y_new = current_x;
-	current_theta = current_theta - data_t(1.5708);
+ data_t pi_over_2 = 1.5708;
+ if (current_y < 0) {
+  x_new = -current_y;
+  y_new = current_x;
+  current_theta = current_theta - pi_over_2;
  } else {
-    x_new = current_y;
-    y_new = -current_x;
-	current_theta = current_theta + data_t(1.5708);
+  x_new = current_y;
+  y_new = -current_x;
+  current_theta = current_theta + pi_over_2;
  }
  current_x = x_new;
  current_y = y_new;
@@ -30,7 +32,7 @@ void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta)
  for(int i = 0; i < NO_ITER; i++)
  {
   
-  if(current_y < data_t(0))
+  if(current_y < 0)
   {
    x_new = current_x - (current_y * Kvalues[i]);
    y_new = current_y + (current_x * Kvalues[i]);
--- project_files/project2/cordic/HLS/cordic/cordiccart2pol.h
+++ project_files/project2/cordic/HLS/cordic/cordiccart2pol.h
@@ -1,14 +1,20 @@
 #ifndef CORDICCART2POL_H
 #define CORDICCART2POL_H
-#include "ap_int.h"
+
+#include "ap_fixed.h"
 
 #define NO_ITER 16
 
-typedef int   coef_t;
-//typedef float data_t;
-typedef float acc_t;
-typedef ap_fixed<12, 3> data_t;
 
-void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta);
+typedef ap_fixed<16, 3> data_t;
+
+// Test values: 4, 8, 12, 16, 20, 32
+#ifndef TABLE_BITS
+#define TABLE_BITS 16
+#endif
+
+typedef ap_fixed<TABLE_BITS, 1> table_t;
+
+void cordiccart2pol(data_t x, data_t y, data_t * r, data_t * theta);
 
 #endif
