daa71828e38715acfb22fa6c13ecc7be57e31d8d
Justin <zeh003@ucsd.edu>
2025-10-21 17:11:36 -0700

    Project 2 Cordic

--- project_files/project2/cordic/HLS/cordic/cordiccart2pol.h
+++ project_files/project2/cordic/HLS/cordic/cordiccart2pol.h
@@ -1,11 +1,14 @@
 #ifndef CORDICCART2POL_H
 #define CORDICCART2POL_H
+#include "ap_int.h"
 
 #define NO_ITER 16
 
 typedef int   coef_t;
-typedef float data_t;
+//typedef float data_t;
 typedef float acc_t;
+typedef ap_fixed<16, 3> data_t;   
+//typedef ap_fixed<16, 3> angle_t; 
 
 void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta);
 
