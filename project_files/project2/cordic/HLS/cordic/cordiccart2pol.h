8c779a221a7bb0374722aa9cdae2593c1420a923
Justin <zeh003@ucsd.edu>
2025-10-23 23:14:11 -0700

    Go back to 1a version

--- project_files/project2/cordic/HLS/cordic/cordiccart2pol.h
+++ project_files/project2/cordic/HLS/cordic/cordiccart2pol.h
@@ -1,20 +1,12 @@
 #ifndef CORDICCART2POL_H
 #define CORDICCART2POL_H
 
-#include "ap_fixed.h"
+#define NO_ITER 20
 
-#define NO_ITER 16
+typedef int   coef_t;
+typedef float data_t;
+typedef float acc_t;
 
-
-typedef ap_fixed<16, 3> data_t;
-
-// Test values: 4, 8, 12, 16, 20, 32
-#ifndef TABLE_BITS
-#define TABLE_BITS 4
-#endif
-
-typedef ap_fixed<TABLE_BITS, 2> table_t;
-
-void cordiccart2pol(data_t x, data_t y, data_t * r, data_t * theta);
+void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta);
 
 #endif
