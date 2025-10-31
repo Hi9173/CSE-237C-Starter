aa22e7a97eaeae91f6d025e04f1732766d133e60
Justin <zeh003@ucsd.edu>
2025-10-31 15:55:53 -0700

    q6

--- project_files/project3/dft_1024_precomputed/dft.cpp
+++ project_files/project3/dft_1024_precomputed/dft.cpp
@@ -1,28 +1,36 @@
-#include <math.h>
 #include "dft.h"
-#include "coefficients1024.h"
+#include"coefficients1024.h"
 
-void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],
-         DTYPE real_op[SIZE], DTYPE imag_op[SIZE])
+
+void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],DTYPE real_op[SIZE],DTYPE imag_op[SIZE])
 {
-    int i, j;            // integers for loop counters
-    DTYPE w, c, s;       // floats for math operations
-
-    for (i = 0; i < SIZE; i++) {
-        real_op[i] = 0;
-        imag_op[i] = 0;
-
-        // Compute angular frequency for this bin
-        w = (DTYPE)(2.0f * 3.14159265358979323846f / (float)SIZE) * (float)i;
-
-        for (j = 0; j < SIZE; j++) {
-            // Compute cosine and sine for this index
-            c = cosf((float)j * w);
-            s = -sinf((float)j * w);
-
-            // Perform DFT accumulation
-            real_op[i] += real_sample[j] * c - imag_sample[j] * s;
-            imag_op[i] += real_sample[j] * s + imag_sample[j] * c;
-        }
-    }
+ int i, j;
+ int idx;
+ DTYPE c, s;
+ 
+//  for (i = 0; i < SIZE; i += 1) {
+//   real_op[i] = 0;
+//   imag_op[i] = 0;
+//   for (j = 0; j < SIZE; j += 1) {
+//    idx = (i * j) % SIZE;
+//    c = cos_coefficients_table[idx];
+//    s = sin_coefficients_table[idx];
+//    real_op[i] += (real_sample[j] * c - imag_sample[j] * s);
+//    imag_op[i] += (real_sample[j] * s + imag_sample[j] * c);
+//   }
+//  }
+ 
+ DTYPE w;
+ for (i = 0; i < SIZE; i += 1) {
+  real_op[i] = 0;
+  imag_op[i] = 0;
+  w = (2.0 * 3.141592653589 / SIZE) * (DTYPE)i;
+  for (j = 0; j < SIZE; j += 1) {
+   c = cos(j * w);
+   s = -sin(j * w);
+   real_op[i] += (real_sample[j] * c - imag_sample[j] * s);
+   imag_op[i] += (real_sample[j] * s + imag_sample[j] * c);
+  }
+ }
+ 
 }
