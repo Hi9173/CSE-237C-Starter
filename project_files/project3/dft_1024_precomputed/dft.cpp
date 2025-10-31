69a997a0d409ee42bcddff2b139e1f940f891f1a
Justin <zeh003@ucsd.edu>
2025-10-31 15:53:36 -0700

    q6

--- project_files/project3/dft_1024_precomputed/dft.cpp
+++ project_files/project3/dft_1024_precomputed/dft.cpp
@@ -3,24 +3,26 @@
 #include "coefficients1024.h"
 
 void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],
-         DTYPE real_op[SIZE],   DTYPE imag_op[SIZE])
+         DTYPE real_op[SIZE], DTYPE imag_op[SIZE])
 {
-    int i, j;
-    DTYPE w, c, s;
+    int i, j;            // integers for loop counters
+    DTYPE w, c, s;       // floats for math operations
 
     for (i = 0; i < SIZE; i++) {
         real_op[i] = 0;
         imag_op[i] = 0;
 
-        w = (2.0 * 3.14159265358979323846 / SIZE) * (DTYPE)i;
+        // Compute angular frequency for this bin
+        w = (DTYPE)(2.0f * 3.14159265358979323846f / (float)SIZE) * (float)i;
 
         for (j = 0; j < SIZE; j++) {
-            c = cos(j * w);
-            s = -sin(j * w);
+            // Compute cosine and sine for this index
+            c = cosf((float)j * w);
+            s = -sinf((float)j * w);
 
+            // Perform DFT accumulation
             real_op[i] += real_sample[j] * c - imag_sample[j] * s;
             imag_op[i] += real_sample[j] * s + imag_sample[j] * c;
         }
     }
 }
-
