fda29eb3add5bacf3a4a021324213aedef559f61
Justin <zeh003@ucsd.edu>
2025-10-31 16:24:45 -0700

    q6

--- project_files/project3/dft_1024_precomputed/dft.cpp
+++ project_files/project3/dft_1024_precomputed/dft.cpp
@@ -13,21 +13,21 @@ void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],DTYPE real_op[SIZE],DT
  
  // Question 6a: Baseline implementation with math functions
  // Use double precision for calculations, then convert to DTYPE (float)
- for (i = 0; i < SIZE; i += 1) {
-  real_op[i] = 0;
-  imag_op[i] = 0;
-  w_double = (2.0 * PI / SIZE) * (double)i;
-  for (j = 0; j < SIZE; j += 1) {
-   c_double = cos((double)j * w_double);
-   s_double = -sin((double)j * w_double);
-   c = (DTYPE)c_double;
-   s = (DTYPE)s_double;
-   real_op[i] += (real_sample[j] * c - imag_sample[j] * s);
-   imag_op[i] += (real_sample[j] * s + imag_sample[j] * c);
-  }
- }
+//  for (i = 0; i < SIZE; i += 1) {
+//   real_op[i] = 0;
+//   imag_op[i] = 0;
+//   w_double = (2.0 * PI / SIZE) * (double)i;
+//   for (j = 0; j < SIZE; j += 1) {
+//    c_double = cos((double)j * w_double);
+//    s_double = -sin((double)j * w_double);
+//    c = (DTYPE)c_double;
+//    s = (DTYPE)s_double;
+//    real_op[i] += (real_sample[j] * c - imag_sample[j] * s);
+//    imag_op[i] += (real_sample[j] * s + imag_sample[j] * c);
+//   }
+//  }
  
- /* Question 6b: Using 1D lookup tables (commented out)
+//Question 6b: Using 1D lookup tables (commented out)
  int idx;
  for (i = 0; i < SIZE; i += 1) {
   real_op[i] = 0;
@@ -40,5 +40,5 @@ void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],DTYPE real_op[SIZE],DT
    imag_op[i] += (real_sample[j] * s + imag_sample[j] * c);
   }
  }
- */
+ 
 }
