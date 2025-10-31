ff4fd97224b0b8adc403bd60d26288b507c8228e
Justin <zeh003@ucsd.edu>
2025-10-31 16:09:23 -0700

    q6

--- project_files/project3/dft_1024_precomputed/dft.cpp
+++ project_files/project3/dft_1024_precomputed/dft.cpp
@@ -1,29 +1,38 @@
-#include <math.h>
+#include<math.h>
 #include "dft.h"
-#include "coefficients1024.h"  // not used here, ok to keep
+#include"coefficients1024.h"
 
-void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],
-         DTYPE real_op[SIZE],   DTYPE imag_op[SIZE])
+
+void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],DTYPE real_op[SIZE],DTYPE imag_op[SIZE])
 {
-    int i, j;
-    DTYPE w, c, s;
-
-    const DTYPE twoPiOverN = (DTYPE)(2.0f * 3.14159265358979323846f / (float)SIZE);
-
-    for (i = 0; i < SIZE; i++) {
-        real_op[i] = 0.0f;
-        imag_op[i] = 0.0f;
-
-        // w = 2π * i / N  (all in float)
-        w = twoPiOverN * (DTYPE)i;
-
-        for (j = 0; j < SIZE; j++) {
-            DTYPE jw = (DTYPE)j * w;
-            c = cosf(jw);
-            s = -sinf(jw);  // forward DFT
-
-            real_op[i] += real_sample[j] * c - imag_sample[j] * s;
-            imag_op[i] += real_sample[j] * s + imag_sample[j] * c;
-        }
-    }
+ int i, j;
+ int idx;
+ DTYPE c, s;
+ 
+ // for (i = 0; i < SIZE; i += 1) {
+ //  real_op[i] = 0;
+ //  imag_op[i] = 0;
+ //  for (j = 0; j < SIZE; j += 1) {
+ //   idx = (i * j) % SIZE;
+ //   c = cos_coefficients_table[idx];
+ //   s = sin_coefficients_table[idx];
+ //   real_op[i] += (real_sample[j] * c - imag_sample[j] * s);
+ //   imag_op[i] += (real_sample[j] * s + imag_sample[j] * c);
+ //  }
+ // }
+ 
+ //Question 6a: Baseline implementation with math functions
+ DTYPE w;
+ for (i = 0; i < SIZE; i += 1) {
+  real_op[i] = 0;
+  imag_op[i] = 0;
+  w = (2.0 * 3.141592653589 / SIZE) * (DTYPE)i;
+  for (j = 0; j < SIZE; j += 1) {
+   c = cos(j * w);
+   s = sin(j * w);
+   real_op[i] += (real_sample[j] * c + imag_sample[j] * s);
+   imag_op[i] += (imag_sample[j] * c - real_sample[j] * s);
+  }
+ }
+ 
 }
