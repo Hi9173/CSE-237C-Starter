26a19d56f1a852f37b77f44968733fb82547e062
Justin <zeh003@ucsd.edu>
2025-10-31 13:37:34 -0700

    dft 32

--- project_files/project3/dft_32_precomputed/dft.cpp
+++ project_files/project3/dft_32_precomputed/dft.cpp
@@ -2,7 +2,28 @@
 #include "dft.h"
 #include"coefficients32.h"
 
-void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE])
+void dft(DTYPE sample_real[SIZE], DTYPE sample_imag[SIZE])
 {
-//Write your code here
+ int i, j;
+ DTYPE w;
+ DTYPE c, s;
+ 
+ DTYPE temp_real[SIZE];
+ DTYPE temp_imag[SIZE];
+ 
+ for (i = 0; i < SIZE; i+= 1) {
+  temp_real[i] = 0;
+  temp_imag[i] = 0;
+  w = (2.0 * 3.141592653589 / SIZE) * (DTYPE)i;
+  for (j = 0; j < SIZE; j += 1) {
+   c = cos(j * w);
+   s = -sin(j * w);
+   temp_real[i] += (sample_real[j] * c - sample_imag[j] * s);
+   temp_imag[i] += (sample_real[j] * s + sample_imag[j] * c);
+  }
+ }
+ for (i = 0; i < SIZE; i+= 1) {
+  sample_real[i] = temp_real[i];
+  sample_imag[i] = temp_imag[i];
+ }
 }
