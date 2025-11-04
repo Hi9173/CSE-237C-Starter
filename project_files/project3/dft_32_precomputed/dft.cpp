fd4ee969e23ec17d3ed9acd9c93e3633422632a0
Justin <zeh003@ucsd.edu>
2025-11-04 14:43:11 -0800

    project3 q7

--- project_files/project3/dft_32_precomputed/dft.cpp
+++ project_files/project3/dft_32_precomputed/dft.cpp
@@ -1,30 +1,34 @@
+#include<math.h>
 #include "dft.h"
-#include"coefficients32_2D.h"
+#include"coefficients32.h"
 
-void dft(DTYPE input_R[SIZE], DTYPE input_I[SIZE], DTYPE output_R[SIZE], DTYPE output_I[SIZE])
+
+void dft(DTYPE sample_real[SIZE], DTYPE sample_imag[SIZE])
 {
-	#pragma HLS ARRAY_PARTITION variable=input_R type=block factor=32
-	#pragma HLS ARRAY_PARTITION variable=input_I type=block factor=32
-	#pragma HLS ARRAY_PARTITION variable=output_R type=block factor=32
-	#pragma HLS ARRAY_PARTITION variable=output_I type=block factor=32
-	#pragma HLS ARRAY_PARTITION variable=cos_coeff_table type=block factor=32 dim=2
-	#pragma HLS ARRAY_PARTITION variable=sin_coeff_table type=block factor=32 dim=2
-	
 	int i, j;
+	DTYPE w;
 	DTYPE c, s;
 	
+	DTYPE temp_real[SIZE];
+	DTYPE temp_imag[SIZE];
+	
 	for (i = 0; i < SIZE; i+= 1) {
-		output_R[i] = 0;
-		output_I[i] = 0;
+		temp_real[i] = 0;
+		temp_imag[i] = 0;
+
+		w = (2.0 * 3.141592653589 / SIZE) * (DTYPE)i;
 
 		for (j = 0; j < SIZE; j += 1) {
 
-			#pragma HLS UNROLL factor=1
-			c = cos_coeff_table[i][j];
-			s = sin_coeff_table[i][j];
-
-			output_R[i] += (input_R[j] * c - input_I[j] * s);
-			output_I[i] += (input_R[j] * s + input_I[j] * c);
+			c = cos(j * w);
+			s = -sin(j * w);
+            
+			temp_real[i] += (sample_real[j] * c - sample_imag[j] * s);
+			temp_imag[i] += (sample_real[j] * s + sample_imag[j] * c);
 		}
+	}
+	for (i = 0; i < SIZE; i+= 1) {
+		sample_real[i] = temp_real[i];
+		sample_imag[i] = temp_imag[i];
 	}
 }
