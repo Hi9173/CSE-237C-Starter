a64870e5c82310f5076a944b2096795864773216
Justin <zeh003@ucsd.edu>
2025-11-04 14:54:40 -0800

    project3 q7

--- project_files/project3/dft_1024_precomputed/dft.cpp
+++ project_files/project3/dft_1024_precomputed/dft.cpp
@@ -21,7 +21,7 @@ void dft(DTYPE sample_real[SIZE], DTYPE sample_imag[SIZE])
 		for (j = 0; j < SIZE; j += 1) {
 
 			c = cosf(j * w);
-			s = sinf(j * w);
+			s = -sinf(j * w);
             
 			temp_real[i] += (sample_real[j] * c - sample_imag[j] * s);
 			temp_imag[i] += (sample_real[j] * s + sample_imag[j] * c);
