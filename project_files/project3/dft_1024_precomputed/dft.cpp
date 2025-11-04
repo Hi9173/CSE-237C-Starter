401d3f058449e35c09ac1b2935302cc7882137e1
Justin <zeh003@ucsd.edu>
2025-11-04 15:43:06 -0800

    project3 q8

--- project_files/project3/dft_1024_precomputed/dft.cpp
+++ project_files/project3/dft_1024_precomputed/dft.cpp
@@ -1,29 +1,28 @@
-#include <math.h>
+#include<math.h>
 #include "dft.h"
-#include "coefficients1024.h"
+#include"coefficients1024.h"
 
-// Question 6a: Baseline DFT1024 using sin() and cos() math functions (no HLS pragmas)
-void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], DTYPE real_op[SIZE], DTYPE imag_op[SIZE])
+void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE],DTYPE real_op[SIZE],DTYPE imag_op[SIZE])
 {
-    int i, j;
-    const double PI = 3.14159265358979323846264338327950288419716939937510;
-    double w_double;
-    double c_double, s_double;
-
-    for (i = 0; i < SIZE; i += 1) {
-        real_op[i] = 0;
-        imag_op[i] = 0;
-
-        for (j = 0; j < SIZE; j += 1) {
-            // compute angle (2π * i * j / SIZE)
-            w_double = (2.0 * PI * (double)i * (double)j) / (double)SIZE;
-
-            // compute cos and sin directly
-            c_double = cos(w_double);
-            s_double = -sin(w_double);
-
-            real_op[i] += (real_sample[j] * (DTYPE)c_double - imag_sample[j] * (DTYPE)s_double);
-            imag_op[i] += (real_sample[j] * (DTYPE)s_double + imag_sample[j] * (DTYPE)c_double);
-        }
+ int i, j;
+ const double PI = 3.14159265358979323846264338327950288419716939937510;
+ double w_double;
+ double c_double, s_double;
+ DTYPE c, s;
+ int idx;
+ 
+ for (i = 0; i < SIZE; i += 1) {
+    #pragma HLS UNROLL factor=32   
+    real_op[i] = 0;
+    imag_op[i] = 0;
+    for (j = 0; j < SIZE; j += 1) {
+        #pragma HLS UNROLL factor=32   
+        idx = (i * j) % SIZE;
+        c = cos_coefficients_table[idx];
+        s = sin_coefficients_table[idx];
+        real_op[i] += (real_sample[j] * c - imag_sample[j] * s);
+        imag_op[i] += (real_sample[j] * s + imag_sample[j] * c);
     }
+ }
+ 
 }
