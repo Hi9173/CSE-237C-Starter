cf8d43b3a87b4a72523571dbb93b1760cb007971
Justin <zeh003@ucsd.edu>
2025-11-07 16:24:44 -0800

    project3 demo

--- project_files/project3/dft_1024_precomputed/dft.cpp
+++ project_files/project3/dft_1024_precomputed/dft.cpp
@@ -11,7 +11,7 @@ void dft(hls::stream<transPkt> &input_real, hls::stream<transPkt> &input_imag,
  #pragma HLS INTERFACE axis port=input_imag
  #pragma HLS INTERFACE axis port=output_real
  #pragma HLS INTERFACE axis port=output_imag
- #pragma HLS INTERFACE ap_ctrl_none port=return
+ #pragma HLS INTERFACE mode=s_axilite port=return
  
  int i, j;
  int idx;
